89c7b8e937ec9c35ab7aa7a9f8ca76f07aae6347
jiejietop <1161959934@qq.com>
2019-05-21 15:48:45 +0800

    更新lwip_udpecho_raw例程

---
+++ lwip_udpecho_raw/User/udpecho.c
@@ -0,0 +1,70 @@
+#include "udpecho.h"
+#include "lwip/netif.h"
+#include "lwip/ip.h"
+#include "lwip/tcp.h"
+#include "lwip/init.h"
+#include "netif/etharp.h"
+#include "lwip/udp.h"
+#include "lwip/pbuf.h"
+#include <stdio.h>	
+
+
+static void udp_demo_callback(void *arg, struct udp_pcb *upcb, struct pbuf *p, const ip_addr_t *addr, u16_t port)
+{
+   struct pbuf *q = NULL;
+   const char* reply = "This is reply!\n";
+
+   if(arg)
+   {
+       printf("%s",(char *)arg);
+   }
+
+   pbuf_free(p);
+   
+   q = pbuf_alloc(PBUF_TRANSPORT, strlen(reply)+1, PBUF_RAM);
+   if(!q)
+   {
+   	   printf("out of PBUF_RAM\n");
+	   return;
+   }
+
+   memset(q->payload, 0 , q->len);
+   memcpy(q->payload, reply, strlen(reply));
+   udp_sendto(upcb, q, addr, port);
+   pbuf_free(q);
+}
+
+static char * st_buffer= "We get a data\n";
+void UDP_Echo_Init(void)
+{
+    struct udp_pcb *udpecho_pcb;
+    /* 新建一个控制块*/      
+    udpecho_pcb = udp_new();    
+
+    /* 绑定端口号 */
+    udp_bind(udpecho_pcb, IP_ADDR_ANY, UDP_ECHO_PORT);
+    
+    /* 注册接收数据回调函数 */
+    udp_recv(udpecho_pcb, udp_demo_callback, (void *)st_buffer);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ lwip_udpecho_raw/User/udpecho.h
@@ -0,0 +1,10 @@
+#ifndef _UDPECHO_H_
+#define _UDPECHO_H_
+
+#define UDP_ECHO_PORT 5001
+
+void UDP_Echo_Init(void);
+
+#endif
+
+
