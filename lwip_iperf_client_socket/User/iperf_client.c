829d2aae53b39eea6bc8bbaf8e3618ea1943c601
jiejietop <1161959934@qq.com>
2019-05-21 12:09:28 +0800

    更新lwip_iperf_client_socket例程

--- lwip_iperf_client_socket/User/iperf_client.c
+++ lwip_iperf_client_socket/User/iperf_client.c
@@ -35,61 +35,58 @@
 
 #include "lwip/sys.h"
 #include "lwip/api.h"
+#include <lwip/sockets.h>
 
-#define IPERF_PORT          5001
+#define PORT              5001
+#define IP_ADDR        "192.168.0.181"
+
 #define IPERF_BUFSZ         (4 * 1024)
-
 
 static void iperf_client(void *thread_param)
 {
-  struct netconn *conn;
-  
-  int i;
-
-  int ret;
-
-  uint8_t *send_buf;
-  
+  int sock = -1,i;
+  struct sockaddr_in client_addr;
+  uint8_t* send_buf;
+  u32_t tick1, tick2;
   uint64_t sentlen;
   
-  u32_t tick1, tick2;
-  ip4_addr_t ipaddr;
-
-
   send_buf = (uint8_t *) pvPortMalloc(IPERF_BUFSZ);
-  if (!send_buf) return ;
-
-  for (i = 0; i < IPERF_BUFSZ; i ++)
-      send_buf[i] = i & 0xff;
+  if (!send_buf) 
+    return ;
+  
+  for(i = 0; i < IPERF_BUFSZ; i ++)
+    send_buf[i] = i & 0xff;
   
   while(1)
   {
-    conn = netconn_new(NETCONN_TCP);
-    if (conn == NULL)
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock < 0)
     {
-      printf("create conn failed!\n");
+      printf("Socket error\n");
       vTaskDelay(10);
       continue;
-    }
-    
-    IP4_ADDR(&ipaddr,192,168,0,181);
-    
-    ret = netconn_connect(conn,&ipaddr,5001);
-    if (ret == -1)
+    } 
+
+    client_addr.sin_family = AF_INET;      
+    client_addr.sin_port = htons(PORT);   
+    client_addr.sin_addr.s_addr = inet_addr(IP_ADDR);
+    memset(&(client_addr.sin_zero), 0, sizeof(client_addr.sin_zero));    
+
+    if (connect(sock, 
+               (struct sockaddr *)&client_addr, 
+                sizeof(struct sockaddr)) == -1) 
     {
         printf("Connect failed!\n");
-        netconn_close(conn);
+        closesocket(sock);
         vTaskDelay(10);
         continue;
-    }
+    }                                           
     
     printf("Connect to iperf server successful!\n");
-    
     tick1 = sys_now();
     while (1)
     {
       tick2 = sys_now();
-      
       if(tick2 - tick1 >= configTICK_RATE_HZ * 5)
       {     
         float f;
@@ -99,21 +96,24 @@ static void iperf_client(void *thread_param)
         
         tick1 = tick2;
         sentlen = 0;
-      }
-      ret = netconn_write(conn,send_buf,IPERF_BUFSZ,0);
-      if (ret == ERR_OK)
-      {
-          sentlen += IPERF_BUFSZ;
-      }
+      }  
       
+      if(write(sock,send_buf,IPERF_BUFSZ) < 0)
+        break;
+      else
+      {
+        sentlen += IPERF_BUFSZ;
+      }
     }
-//    netconn_close(conn);
-//    netconn_delete(conn);
+    closesocket(sock);
   }
+
 }
+
+
 
 void
 iperf_client_init(void)
 {
-  sys_thread_new("iperf_client", iperf_client, NULL, 512, 4);
+  sys_thread_new("iperf_client", iperf_client, NULL, 1024, 8);
 }
