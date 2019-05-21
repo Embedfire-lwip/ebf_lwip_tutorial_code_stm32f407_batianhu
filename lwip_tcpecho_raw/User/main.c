4cbc77be06f318f8c134537a4bd608f630dfc00e
jiejietop <1161959934@qq.com>
2019-05-21 15:46:44 +0800

    更新lwip_tcpecho_raw例程

---
+++ lwip_tcpecho_raw/User/main.c
@@ -0,0 +1,114 @@
+/**
+  *********************************************************************
+  * @file    main.c
+  * @author  fire
+  * @version V1.0
+  * @date    2019-xx-xx
+  * @brief   FreeRTOS V9.0.0 + STM32 LwIP
+  *********************************************************************
+  * @attention
+  *
+  * 实验平台:野火  STM32全系列开发板 
+  * 论坛    :http://www.firebbs.cn
+  * 淘宝    :https://fire-stm32.taobao.com
+  *
+  **********************************************************************
+  */ 
+ 
+/*
+*************************************************************************
+*                             包含的头文件
+*************************************************************************
+*/ 
+#include "main.h"
+
+#include <lwip/opt.h>
+#include <lwip/arch.h>
+#include "tcpip.h"
+#include "lwip/init.h"
+#include "lwip/netif.h"
+#include "ethernetif.h"
+#include "netif/ethernet.h"
+#include "lwip/def.h"
+#include "lwip/stats.h"
+#include "lwip/etharp.h"
+#include "lwip/ip.h"
+#include "lwip/snmp.h"
+#include "lwip/timeouts.h"
+
+#include "tcpecho.h"
+
+struct netif gnetif;
+ip4_addr_t ipaddr;
+ip4_addr_t netmask;
+ip4_addr_t gw;
+uint8_t IP_ADDRESS[4];
+uint8_t NETMASK_ADDRESS[4];
+uint8_t GATEWAY_ADDRESS[4];
+
+void LwIP_Init(void)
+{
+  /* IP addresses initialization */
+  /* USER CODE BEGIN 0 */
+#ifdef USE_DHCP
+  ip_addr_set_zero_ip4(&ipaddr);
+  ip_addr_set_zero_ip4(&netmask);
+  ip_addr_set_zero_ip4(&gw);
+#else
+  IP4_ADDR(&ipaddr,IP_ADDR0,IP_ADDR1,IP_ADDR2,IP_ADDR3);
+  IP4_ADDR(&netmask,NETMASK_ADDR0,NETMASK_ADDR1,NETMASK_ADDR2,NETMASK_ADDR3);
+  IP4_ADDR(&gw,GW_ADDR0,GW_ADDR1,GW_ADDR2,GW_ADDR3);
+#endif /* USE_DHCP */
+  /* USER CODE END 0 */
+    
+  /* Initilialize the LwIP stack without RTOS */
+  lwip_init();
+  
+  /* add the network interface (IPv4/IPv6) without RTOS */
+  netif_add(&gnetif, &ipaddr, &netmask, &gw, NULL, &ethernetif_init, &ethernet_input);
+
+  /* Registers the default network interface */
+  netif_set_default(&gnetif);
+
+  if (netif_is_link_up(&gnetif))
+  {
+    /* When the netif is fully configured this function must be called */
+    netif_set_up(&gnetif);
+  }
+  else
+  {
+    /* When the netif link is down this function must be called */
+    netif_set_down(&gnetif);
+  }
+
+/* USER CODE BEGIN 3 */
+
+/* USER CODE END 3 */
+}
+
+
+int flag = 0;
+int main(void)
+{
+  //板级外设初始化
+  BSP_Init();
+  
+  //LwIP协议栈初始化
+  LwIP_Init();  
+  TCP_Echo_Init();
+  while (1)
+  {
+    if(flag)
+    {
+      flag = 0;
+      //调用网卡接收函数
+      ethernetif_input(&gnetif);
+    }
+    //处理LwIP中定时事件
+    sys_check_timeouts();
+  }
+}
+
+
+
+/********************************END OF FILE****************************/
